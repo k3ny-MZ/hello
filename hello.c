55caf7da8544d69aa2fc2aadd6e13aa0c24b747a
Ben Smith <bsmith142@unl.edu>
2025-08-29 23:01:16 -0500

    added name and major

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 /**
  * Basic Hello World Program
- */
+ *///added name, major, and minor 
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,7 +11,9 @@
     printf("Major is Computer Science!\n");
     printf("Minor is none\n");
 
-
+    printf("Hello, I'm Ben Smith!\n");
+    printf("Major is Electrical Engineering!\n");
+    printf("Minor is Computer Science!\n");
 
 
     return 0;
